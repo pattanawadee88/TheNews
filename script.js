16ea8899a17c54da7f39f5cd62a7426b3a65c364
pattanawadee88 <winyaratp@wit.edu>
2021-07-22 16:10:13 -0400

    added display date

---
+++ script.js
@@ -0,0 +1,29 @@
+let localDate = new Date();
+const localDateDOM= document.querySelector("time");
+
+/***** Display Date ******/ 
+localDateDOM.innerHTML = localDate.toDateString();
+
+
+
+// window.onload =  getnews();
+
+    const newsHeadline = document.querySelector('.news-headline');
+    const newsContent = document.querySelector('.news-content');
+    const btn = document.querySelector('.read-more-btn');
+    const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=G9k66bXLmyMt6vv61ul58QwnTAau9ItH';
+
+    // make an Ajax request
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState ===4){
+            let news = JSON.parse(xhr.responseText)
+            console.log(news);
+            
+            newsHeadline.innerHTML = news.results[5].title;
+            newsContent.innerHTML = news.results[5].abstract;
+        }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+    
