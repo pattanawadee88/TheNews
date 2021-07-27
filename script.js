c7ec10eb01a682712dc1519271da7da72fe966c2
pattanawadee88 <winyaratp@wit.edu>
2021-07-27 15:40:36 -0400

    rewritten requestData function

--- script.js
+++ script.js
@@ -4,31 +4,6 @@ const localDateDOM= document.querySelector("time");
 /***** Display Date ******/ 
 localDateDOM.innerHTML = localDate.toDateString();
 
-
-
-
-// window.onload =  getnews();
-
-    const newsHeadline = document.querySelector('.news-headline');
-    const newsContent = document.querySelector('.news-content');
-    const btn = document.querySelector('.read-more-btn');
-    const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=G9k66bXLmyMt6vv61ul58QwnTAau9ItH';
-
-    // make an Ajax request
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState ===4){
-            let news = JSON.parse(xhr.responseText)
-            console.log(news);
-            
-            newsHeadline.innerHTML = news.results[5].title;
-            newsContent.innerHTML = news.results[5].abstract;
-        }
-    };
-    xhr.open('GET', url);
-    xhr.send();
-    
-
 // Display Nav Bar
 const nav = document.querySelector('.shownav');
 
@@ -36,3 +11,43 @@ function displayNav(){
     nav.style.display = 'block';
     console.log('hello')
 }
+
+
+// window.onload =  getnews();
+const newsHeadline = document.querySelector('.news-headline');
+const newsContent = document.querySelector('.news-content');
+const btn = document.querySelector('.read-more-btn');
+const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=G9k66bXLmyMt6vv61ul58QwnTAau9ItH';
+
+// make an Ajax request
+function requestData(){
+    return new Promise((resolve,reject)=> {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function(){
+            if(xhr.readyState ===4){
+                let data = JSON.parse(xhr.responseText)
+                resolve(data);
+            }else {
+                reject(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = ()=> reject(Error('A network error occured'));
+        xhr.send();
+    });
+} 
+// A news generate HTML function
+function generateNewsHTML(){
+   
+}   
+requestData()
+    .then( data => {
+    newsHeadline.innerHTML = data.results[5].title;
+            // newsHeadline.innerHTML = news.results[5].title;
+    newsContent.innerHTML = data.results[5].abstract;
+
+     console.log(data)})
+     .catch( err => console.log(err))
+
+    
+
