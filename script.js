82881e198e2aac019d3aa5e16d4df7bf559be908
pattanawadee88 <winyaratp@wit.edu>
2021-08-14 16:30:46 -0400

    add generateNewsHTML function

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const newsContent = document.querySelector('.news-content');
 const newsImg = document.querySelector('.news-img');
 const btn = document.querySelector('.read-more-btn');
 const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=G9k66bXLmyMt6vv61ul58QwnTAau9ItH';
-
+const newsContainer = document.querySelector('.news-container');
 // make an Ajax request
 function requestData(){
     return new Promise((resolve,reject)=> {
@@ -29,6 +29,8 @@ function requestData(){
             if(xhr.readyState ===4){
                 let data = JSON.parse(xhr.responseText)
                 resolve(data);
+                console.log(data);
+
             }else {
                 reject(Error(xhr.statusText));
             }
@@ -38,16 +40,43 @@ function requestData(){
     });
 } 
 // A news generate HTML function
-function generateNewsHTML(){
-   
+function generateNewsHTML(data){
+    
+    for(let i = 0; i < data.results.length; i++){
+        //create section node
+        let nodeSection = document.createElement("SECTION"); 
+        nodeSection.setAttribute("class", "news-container");
+        document.body.appendChild(nodeSection);
+        //create image node
+        let nodeImg = document.createElement("IMG");  
+        nodeImg.setAttribute('class','news-img');
+        nodeImg.setAttribute('src', data.results[i].multimedia[0].url )
+        nodeSection.appendChild(nodeImg);
+        //create header node
+        let newsheadline = document.createElement("HEADER");   
+        newsheadline.setAttribute("class","news-headline")          
+        let newsheader = document.createTextNode(data.results[i].title); 
+        newsheadline.appendChild(newsheader);                                          
+        nodeSection.appendChild(newsheadline);
+        //create article node
+        let nodeArticle = document.createElement("ARTICLE"); 
+        nodeArticle.setAttribute("class","news-content")                       
+        let articleText = document.createTextNode(data.results[i].abstract); 
+        nodeArticle.appendChild(articleText);                                          
+        nodeSection.appendChild(nodeArticle);
+        //create button node
+        let nodeBtn = document.createElement("button"); 
+        nodeBtn.setAttribute("class","read-more-btn");
+        nodeBtn.innerHTML = "Read more";
+        nodeSection.appendChild(nodeBtn);
+    }
+    
 }   
 requestData()
     .then( data => {
-    newsHeadline.innerHTML = data.results[5].title;
-    newsImg.src = data.results[5].multimedia[0].url;
-    newsContent.innerHTML = data.results[5].abstract;
-
-     console.log(data)})
+        generateNewsHTML(data)
+    
+    })
      .catch( err => console.log(err))
 
     
